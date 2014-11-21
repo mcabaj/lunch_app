27d3b0196a18ab8be84e155813bb552fa6c23a17
Marek Cabaj <maca@grapesoftware.com>
2014-11-21 16:42:21 +0100

    Added default empty constructor for Jackson mapper

--- src/main/java/com/grapeup/domain/User.java
+++ src/main/java/com/grapeup/domain/User.java
@@ -14,9 +14,7 @@ public class User {
     private String firstName;
     private String lastName;
 
-    public User(String firstname, String lastname) {
-        this.firstName = firstname;
-        this.lastName = lastname;
+    public User() {
     }
 
     public String getFirstName() {
