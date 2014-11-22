e7d209e7ea20bc00bf63f2e1b931ced73e6349d6
Marek Cabaj <maca@grapesoftware.com>
2014-11-22 04:52:05 +0100

    Permited /websocket url to all

--- src/main/java/com/grapeup/configs/SecurityConfig.java
+++ src/main/java/com/grapeup/configs/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
-        http.authorizeRequests().antMatchers("/login")
-            .permitAll();
+        http.authorizeRequests().antMatchers("/login").permitAll();
+        http.authorizeRequests().antMatchers("/websocket").permitAll();
 
         http.authorizeRequests().anyRequest()
             .authenticated()
